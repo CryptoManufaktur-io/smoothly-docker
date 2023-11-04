865374dc6b74a68820a0d345dfdb162741e6bc1d
yorickdowne <yorickdowne@gmail.com>
2023-11-04 15:58:24 +0000

    Spelling

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-_public_ip="-ip $(wget -qO- https://ifconfig.me/ip)"
+__public_ip="-ip $(wget -qO- https://ifconfig.me/ip)"
 
 # Word splitting is desired for the command line parameters
 # shellcheck disable=SC2086
